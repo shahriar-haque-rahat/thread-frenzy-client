711aa573e92559d775fe5416d3643a120f1fef62
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-25 01:11:04 +0600

    men collection

--- src/pages/products/Men.jsx
+++ src/pages/products/Men.jsx
@@ -18,37 +18,42 @@ const Men = () => {
     }, [status, dispatch]);
     console.log(menCollections);
     return (
-        <div className=" grid grid-cols-3 gap-3 px-[5%]">
-            {
-                menCollections?.map(item => (
-                    <div key={item._id}>
-                        <Swiper
-                            modules={[Scrollbar, Autoplay]}
-                            spaceBetween={20}
-                            slidesPerView={1}
-                            scrollbar={{ draggable: true }}
-                            autoplay={{ delay: 10000 }}>
+        <div className=" px-[5%]">
+            <div className=" flex justify-center items-center h-40 mb-10 border shadow-lg">
+                <h1 className=" text-4xl">Men Collections</h1>
+            </div>
+            <div className=" grid grid-cols-3 gap-3">
+                {
+                    menCollections?.map(item => (
+                        <div key={item._id}>
+                            <Swiper
+                                modules={[Scrollbar, Autoplay]}
+                                spaceBetween={20}
+                                slidesPerView={1}
+                                scrollbar={{ draggable: true }}
+                                autoplay={{ delay: 10000 }}>
 
-                            <div>
-                                {
-                                    item.images[item.color[0]].map((image, index) => (
-                                        <SwiperSlide key={index}>
-                                            <img className=" h-96 w-full object-cover object-top" src={image} alt={`${item.name} ${item.color[0]} ${index}`} />
-                                        </SwiperSlide>
-                                    ))
-                                }
-                            </div>
-                        </Swiper>
-                        <div className=" p-2">
-                            <h1 className=" text-lg font-semibold">{item.name}</h1>
-                            <div className=" flex justify-between">
-                                <p>Color: {item?.color[0]}</p>
-                                <p>Price: ${item?.price}</p>
+                                <div>
+                                    {
+                                        item.images[item.color[0]].map((image, index) => (
+                                            <SwiperSlide key={index}>
+                                                <img className=" h-96 w-full object-cover object-top" src={image} alt={`${item.name} ${item.color[0]} ${index}`} />
+                                            </SwiperSlide>
+                                        ))
+                                    }
+                                </div>
+                            </Swiper>
+                            <div className=" p-2">
+                                <h1 className=" text-lg font-semibold">{item.name}</h1>
+                                <div className=" flex justify-between">
+                                    <p>Color: {item?.color[0]}</p>
+                                    <p>Price: ${item?.price}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))
-            }
+                    ))
+                }
+            </div>
         </div>
     );
 };

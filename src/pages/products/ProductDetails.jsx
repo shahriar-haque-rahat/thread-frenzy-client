2bfc8f24f389adcaf5a25b4200b75bad848696c5
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-25 21:01:00 +0600

    similar products

--- src/pages/products/ProductDetails.jsx
+++ src/pages/products/ProductDetails.jsx
@@ -38,94 +38,97 @@ const ProductDetails = () => {
     }
 
     return (
-        <div className=" px-[3%]">
-            {
-                selectedItem && (
-                    <div className=" grid grid-cols-5 gap-5">
-                        <div className=" col-span-3 grid grid-cols-2 grid-rows-2 gap-2 h-fit">
-                            {
-                                selectedItem.images[selectedItem.color[colorIndex]]?.map((item, index) => (
-                                    <img key={index} src={item} alt="loading..." className=" h-96 w-full object-cover object-top" />
-                                ))
-                            }
-                        </div>
-
-                        <div className=" col-span-2 space-y-6">
-                            <h1 className=" text-2xl font-semibold">{selectedItem.name}</h1>
-                            
-                            <div>
-                                <ReactStars value={selectedItem.rating} isHalf={true} count={5} size={24} activeColor="#ffd700" edit={false} />
+        <div>
+            <div className=" px-[3%]">
+                {
+                    selectedItem && (
+                        <div className=" grid grid-cols-5 gap-5">
+                            <div className=" col-span-3 grid grid-cols-2 grid-rows-2 gap-2 h-fit">
+                                {
+                                    selectedItem.images[selectedItem.color[colorIndex]]?.map((item, index) => (
+                                        <img key={index} src={item} alt="loading..." className=" h-96 w-full object-cover object-top" />
+                                    ))
+                                }
                             </div>
-                            
-                            <p className=" text-xl text-green-500 font-semibold">${selectedItem.price}</p>
-                            
-                            <ul className="flex gap-3">Colors:
-                                {
-                                    selectedItem.color?.map((item, index) => (
-                                        <li key={index} onClick={() => setColorIndex(index)} className={`cursor-pointer ${index === colorIndex ? 'border-b-2 border-black' : ''}`}>
-                                            {item}
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                            
-                            <ul className=" flex gap-3 items-center">Sizes:
-                                {
-                                    selectedItem.size?.map((item, index) => (
-                                        <button key={index} className=" border h-10 w-12">
-                                            {item}
-                                        </button>
-                                    ))
-                                }
-                            </ul>
-                            
-                            <div className=" flex gap-6 items-center">
-                                <div className="flex items-center gap-4">
-                                    <p onClick={() => handleQuantity("-")} className="w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> - </p>
-                                    <p className="px-5 py-1 font-semibold border ">{productQuantity}</p>
-                                    <p onClick={() => handleQuantity("+")} className=" w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> + </p>
+
+                            <div className=" col-span-2 space-y-6">
+                                <h1 className=" text-2xl font-semibold">{selectedItem.name}</h1>
+
+                                <div>
+                                    <ReactStars value={selectedItem.rating} isHalf={true} count={5} size={24} activeColor="#ffd700" edit={false} />
                                 </div>
+
+                                <p className=" text-xl text-green-500 font-semibold">${selectedItem.price}</p>
+
+                                <ul className="flex gap-3">Colors:
+                                    {
+                                        selectedItem.color?.map((item, index) => (
+                                            <li key={index} onClick={() => setColorIndex(index)} className={`cursor-pointer ${index === colorIndex ? 'border-b-2 border-black' : ''}`}>
+                                                {item}
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+
+                                <ul className=" flex gap-3 items-center">Sizes:
+                                    {
+                                        selectedItem.size?.map((item, index) => (
+                                            <button key={index} className=" border h-10 w-12">
+                                                {item}
+                                            </button>
+                                        ))
+                                    }
+                                </ul>
+
+                                <div className=" flex gap-6 items-center">
+                                    <div className="flex items-center gap-4">
+                                        <p onClick={() => handleQuantity("-")} className="w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> - </p>
+                                        <p className="px-5 py-1 font-semibold border ">{productQuantity}</p>
+                                        <p onClick={() => handleQuantity("+")} className=" w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> + </p>
+                                    </div>
                                     <button className=" bg-black text-white w-full h-10">Add to Cart</button>
-                            </div>
-
-                            <div>
-                                <div className="collapse border-t border-gray-400 rounded-none">
-                                    <input type="checkbox" className="peer" checked={isDetailsOpen} onChange={() => setIsDetailsOpen(!isDetailsOpen)} />
-                                    <div className="collapse-title font-semibold flex items-center justify-between">
-                                        Product Details {isDetailsOpen ? <FaAngleUp /> : <FaAngleDown />}
-                                    </div>
-                                    <div className="collapse-content ">
-                                        <ul className=" flex flex-col list-disc pl-5">
-                                            {
-                                                selectedItem.details?.map((item, index) => (
-                                                    <li key={index}>
-                                                        {item}
-                                                    </li>
-                                                ))
-                                            }
-                                        </ul>
-                                    </div>
                                 </div>
-                                
-                                <div className="collapse border-y border-gray-400 rounded-none">
-                                    <input type="checkbox" className="peer" checked={isShippingOpen} onChange={() => setIsShippingOpen(!isShippingOpen)} />
-                                    <div className="collapse-title font-semibold flex items-center justify-between">
-                                        Shipping and Returns {isShippingOpen ? <FaAngleUp /> : <FaAngleDown />}
+
+                                <div>
+                                    <div className="collapse border-t border-gray-400 rounded-none">
+                                        <input type="checkbox" className="peer" checked={isDetailsOpen} onChange={() => setIsDetailsOpen(!isDetailsOpen)} />
+                                        <div className="collapse-title font-semibold flex items-center justify-between">
+                                            Product Details {isDetailsOpen ? <FaAngleUp /> : <FaAngleDown />}
+                                        </div>
+                                        <div className="collapse-content ">
+                                            <ul className=" flex flex-col list-disc pl-5">
+                                                {
+                                                    selectedItem.details?.map((item, index) => (
+                                                        <li key={index}>
+                                                            {item}
+                                                        </li>
+                                                    ))
+                                                }
+                                            </ul>
+                                        </div>
                                     </div>
-                                    <div className="collapse-content ">
-                                        <ul className=" list-disc pl-5">
-                                            <li>Returns accepted by mail and in store within 30 days of the shipment date. Items must be unworn and tags must be attached.</li>
-                                            <li>$4.99 USD will be deducted from your refund for returns.</li>
-                                            <li>Once a return is received, please allow 7-14 business days to process and 3-5 business days for the refund to be credited.</li>
-                                        </ul>
+
+                                    <div className="collapse border-y border-gray-400 rounded-none">
+                                        <input type="checkbox" className="peer" checked={isShippingOpen} onChange={() => setIsShippingOpen(!isShippingOpen)} />
+                                        <div className="collapse-title font-semibold flex items-center justify-between">
+                                            Shipping and Returns {isShippingOpen ? <FaAngleUp /> : <FaAngleDown />}
+                                        </div>
+                                        <div className="collapse-content ">
+                                            <ul className=" list-disc pl-5">
+                                                <li>Returns accepted by mail and in store within 30 days of the shipment date. Items must be unworn and tags must be attached.</li>
+                                                <li>$4.99 USD will be deducted from your refund for returns.</li>
+                                                <li>Once a return is received, please allow 7-14 business days to process and 3-5 business days for the refund to be credited.</li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                )
-            }
-            <SimilarProducts itemBrand={selectedItem?.brand}></SimilarProducts>
+                    )
+                }
+                <h1 className=" text-3xl font-semibold mt-20 mb-10">You may also like</h1>
+                <SimilarProducts itemBrand={selectedItem?.brand} itemId={itemId}></SimilarProducts>
+            </div>
         </div>
     );
 };
--- src/pages/products/SimilarProducts.jsx
+++ src/pages/products/SimilarProducts.jsx
@@ -1,14 +1,20 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { allData } from "../../redux/dataSlice";
-import SliderCards from "./SliderCards";
+import { Scrollbar, Navigation, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/scrollbar';
+import 'swiper/css/autoplay';
+import 'swiper/css/navigation';
+import { Link } from "react-router-dom";
+// import SliderCards from "./SliderCards";
 
-
-const SimilarProducts = ({ itemBrand }) => {
+const SimilarProducts = ({ itemBrand, itemId }) => {
     const dispatch = useDispatch();
     const { data, allDataStatus, error } = useSelector((state) => state.data);
-    const filteredData = data?.filter(item => item.brand === itemBrand);
-    console.log(filteredData);
+    const filteredData = data?.filter(item => item.brand === itemBrand && item._id !== itemId);
+
 
     useEffect(() => {
         if (allDataStatus === 'idle') {
@@ -27,7 +33,59 @@ const SimilarProducts = ({ itemBrand }) => {
 
     return (
         <>
-            <SliderCards data={filteredData}/>
+            <Swiper
+                modules={[Scrollbar, Navigation, Autoplay]}
+                spaceBetween={20}
+                breakpoints={{
+                    320: {
+                        slidesPerView: 2.3,
+                        spaceBetween: 5
+                    },
+                    768: {
+                        slidesPerView: 3.3,
+                        spaceBetween: 15
+                    },
+
+                }}
+                navigation
+                scrollbar={{ draggable: true }}>
+                {
+                    filteredData?.map(item => (
+                        <SwiperSlide key={item._id}>
+                            <div>
+                                <Swiper
+                                    modules={[Scrollbar, Autoplay]}
+                                    spaceBetween={20}
+                                    slidesPerView={1}
+                                    scrollbar={{ draggable: true }}
+                                    autoplay={{ delay: 10000 }}>
+
+                                    <div>
+                                        {
+                                            item.images[item.color[0]]?.map((image, index) => (
+                                                <SwiperSlide key={index} className="relative group">
+                                                    <img className="h-96 w-full object-cover object-top" src={image} alt={`${item.name} ${item.color[0]} ${index}`} />
+                                                    <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 text-white text-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                                        <p className=" mb-4">Price: ${item.price}</p>
+                                                        <Link to={`/product-details/${item._id}`}><button className="relative text-white  text-sm px-3 py-1 after:content-[''] after:absolute after:left-1/2 after:-translate-x-1/2 after:bottom-0 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full before:content-[''] before:absolute before:left-1/2 before:-translate-x-1/2 before:top-0 before:h-0.5 before:w-0 before:bg-white before:transition-all before:duration-300 hover:before:w-full">
+                                                            View Details
+                                                        </button></Link>
+                                                    </div>
+                                                </SwiperSlide>
+                                            ))
+                                        }
+                                    </div>
+                                </Swiper>
+                                <div className=" p-3">
+                                    <h1 className=" text-lg font-semibold">{item.name}</h1>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+
+                    ))
+                }
+            </Swiper>
+            {/* <SliderCards data={filteredData} /> */}
         </>
     );
 };

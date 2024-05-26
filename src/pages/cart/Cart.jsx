98dd9a2c6c595d62745eab53776a1deeb6a66b9b
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-26 23:53:31 +0600

    cart item update

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -9,7 +9,7 @@ const Cart = () => {
     const dispatch = useDispatch();
     const { cartItems, cartStatus, cartError } = useSelector(state => state.cart);
     const [quantities, setQuantities] = useState({});
-
+    const [filteredCart, setFilteredCart] = useState([]);
 
     const handleQuantity = (id, operation) => {
         setQuantities(prevQuantities => {
@@ -29,9 +29,9 @@ const Cart = () => {
         dispatch(deleteCartItem(id))
             .unwrap()
             .then(() => {
-                dispatch(getCart(user.email));
-            })
-    }
+                setFilteredCart(prevFilteredCart => prevFilteredCart.filter(item => item._id !== id));
+            });
+    };
 
     useEffect(() => {
         if (user?.email) {
@@ -41,6 +41,7 @@ const Cart = () => {
 
     useEffect(() => {
         if (cartItems) {
+            setFilteredCart(cartItems);
             const initialQuantities = cartItems.reduce((acc, item) => {
                 acc[item._id] = item.quantity;
                 return acc;
@@ -57,36 +58,34 @@ const Cart = () => {
         return <div>Error: {cartError}</div>;
     }
 
-    const totalPrice = cartItems.reduce((acc, item) => acc + item.price * (quantities[item._id] || 1), 0).toFixed(2);
+    const totalPrice = filteredCart.reduce((acc, item) => acc + item.price * (quantities[item._id] || 1), 0).toFixed(2);
 
     return (
         <div className="px-[3%] grid lg:grid-cols-5 gap-10">
             <div className="lg:col-span-3 space-y-10">
-                {
-                    cartItems?.map(item => (
-                        <div key={item._id} className="flex gap-4 border border-black">
-                            <img className="h-40 w-32 object-cover object-top" src={item.image} alt="loading..." />
-                            <div className="w-full flex flex-col justify-between p-3">
-                                <div className="flex justify-between items-start">
-                                    <div>
-                                        <h1 className="text-xl">{item.name}</h1>
-                                        <p>{item.color}</p>
-                                        <p>Size: {item.size}</p>
-                                    </div>
-                                    <button onClick={() => handleDeleteCartItem(item._id)}><RxCross2 size={25} /></button>
+                {filteredCart.map(item => (
+                    <div key={item._id} className="flex gap-4 border border-black">
+                        <img className="h-40 w-32 object-cover object-top" src={item.image} alt="loading..." />
+                        <div className="w-full flex flex-col justify-between p-3">
+                            <div className="flex justify-between items-start">
+                                <div>
+                                    <h1 className="text-xl">{item.name}</h1>
+                                    <p>{item.color}</p>
+                                    <p>Size: {item.size}</p>
                                 </div>
-                                <div className="flex justify-between items-end">
-                                    <div className="flex items-center gap-4">
-                                        <p onClick={() => handleQuantity(item._id, "-")} className="w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> - </p>
-                                        <p className="px-5 py-1 font-semibold border ">{quantities[item._id]}</p>
-                                        <p onClick={() => handleQuantity(item._id, "+")} className="w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> + </p>
-                                    </div>
-                                    <p className="text-lg font-semibold">${(item.price * quantities[item._id]).toFixed(2)}</p>
+                                <button onClick={() => handleDeleteCartItem(item._id)}><RxCross2 size={25} /></button>
+                            </div>
+                            <div className="flex justify-between items-end">
+                                <div className="flex items-center gap-4">
+                                    <p onClick={() => handleQuantity(item._id, "-")} className="w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> - </p>
+                                    <p className="px-5 py-1 font-semibold border ">{quantities[item._id]}</p>
+                                    <p onClick={() => handleQuantity(item._id, "+")} className="w-[20px] h-[20px] lg:w-[35px] lg:h-[35px] rounded-full flex justify-center items-center text-xl cursor-pointer active:scale-95 duration-300 border"> + </p>
                                 </div>
+                                <p className="text-lg font-semibold">${(item.price * quantities[item._id]).toFixed(2)}</p>
                             </div>
                         </div>
-                    ))
-                }
+                    </div>
+                ))}
             </div>
             <div className="lg:col-span-2 space-y-6">
                 <h1 className="text-xl">Order Summary</h1>
@@ -97,17 +96,12 @@ const Cart = () => {
                     </div>
                     <div className="flex justify-between">
                         <p>Shipping Fees</p>
-                        {
-                            (cartItems.length > 0) ? <p>$14.99</p> : <p>$0.00</p>
-                        }
+                        {filteredCart.length > 0 ? <p>$14.99</p> : <p>$0.00</p>}
                     </div>
                 </div>
                 <div className="flex justify-between px-2 text-lg font-semibold">
                     <p>Subtotal</p>
-                    {
-                        (cartItems.length > 0) ? <p>${(parseFloat(totalPrice) + 14.99).toFixed(2)}</p> : <p>$0.00</p>
-                    }
-                    
+                    {filteredCart.length > 0 ? <p>${(parseFloat(totalPrice) + 14.99).toFixed(2)}</p> : <p>$0.00</p>}
                 </div>
                 <button className="bg-black text-white text-lg font-semibold w-full h-12 mx-2">Check Out</button>
             </div>

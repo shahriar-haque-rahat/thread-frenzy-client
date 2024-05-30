48858bf70290b5111fe7e3b8b68c2580ba34d581
shRahat10 <shahriar.haque.1011@gmail.com>
2024-05-30 20:44:46 +0600

    manage order

--- src/pages/profile/adminComponents/ManageOrders.jsx
+++ src/pages/profile/adminComponents/ManageOrders.jsx
@@ -1,26 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllCart } from "../../../redux/cartSlice";
-import { IoMdDoneAll } from "react-icons/io";
+
 
 
 const ManageOrders = () => {
-    const dispatch = useDispatch();
-    const { allCartItems, allCartStatus, allCartError } = useSelector(state => state.cart);
-
-    useEffect(() => {
-        if (allCartStatus === 'idle') {
-            dispatch(getAllCart());
-        }
-    }, [dispatch, allCartStatus])
-
-    if (allCartStatus === 'loading') {
-        return <div>Loading...</div>;
-    }
-
-    if (allCartStatus === 'failed') {
-        return <div>Error: {allCartError}</div>;
-    }
+    
 
     return (
         <div className="mt-6 mr-6 space-y-6">
@@ -34,23 +16,7 @@ const ManageOrders = () => {
                     <div>Total Price</div>
                     <div className=" text-start">Status</div>
                 </div>
-                {
-                    allCartItems?.map(item => (
-                        <div key={item._id} className=" grid grid-cols-6 gap-3 text-center py-2 border-b border-gray-400">
-                            <div>{item.date.split('T')[0]}</div>
-                            <div className=" overflow-hidden">{item._id}</div>
-                            <div>{item?.transactionId}</div>
-                            <div>{item?.quantity}</div>
-                            <div>{item?.price * item?.quantity}</div>
-                            <div className=" flex justify-between items-center">
-                                <p className={item?.status === "pending" ? "text-orange-500" : "text-green-500"}>{item?.status}</p>
-                                {
-                                    item?.status === "pending" && <button><IoMdDoneAll className=" text-green-600" size={25} /></button>
-                                }
-                            </div>
-                        </div>
-                    ))
-                }
+                {/* TODO: ordered product listing */}
             </div>
         </div>
     );

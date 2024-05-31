ec5e6c2dd3833653bda3ba246cabfc9937875807
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-01 02:14:24 +0600

    error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,22 @@
+import { useLottie } from "lottie-react";
+import error from "../json/error.json";
+import { Link } from "react-router-dom";
 
 
 const Error = () => {
+    const options = {
+        animationData: error,
+        loop: true,
+        style: { height: 100 }
+    };
+
+    const { View } = useLottie(options);
+
     return (
-        <div>
-            
+        <div className=" space-y-4 flex flex-col items-center pt-32 font-clashGrotesk font-medium">
+            <div className=" w-[30%]">{View}</div>
+            <h1 className=" text-2xl ">Unexpected Application Error</h1>
+            <Link to={'/'}><button className=" flex items-center bg-black text-white px-2 py-1 w-fit ">Back to Home</button></Link>
         </div>
     );
 };

b17475f299f7a3c3e3b307d1a249963eca4f204f
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-13 15:19:32 +0600

    navbar

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -6,14 +6,30 @@ import { IoBagOutline } from "react-icons/io5";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AuthContext } from "../../provider/AuthProvider";
 import { toast } from "react-toastify";
+import { useScroll, motion, useMotionValueEvent } from "framer-motion";
 import 'react-toastify/dist/ReactToastify.css';
 
 const Navbar = () => {
     const { user, userSignOut } = useContext(AuthContext);
-    const navbarRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
     const [theme, setTheme] = useState("light");
+    const [hidden, setHidden] = useState(false);
+    const { scrollY } = useScroll();
     const html = document.documentElement;
+    const navbarRef = useRef(null);
+
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme) {
+            setTheme(savedTheme);
+            if (savedTheme === "dark") {
+                html.classList.add("dark");
+            } else {
+                html.classList.remove("dark");
+            }
+        }
+    }, [html.classList]);
 
     const handleUserSignOut = () => {
         userSignOut()
@@ -41,6 +57,15 @@ const Navbar = () => {
         setIsOpen(!isOpen);
     };
 
+    useMotionValueEvent(scrollY, "change", (latest) => {
+        const prevValue = scrollY.getPrevious();
+        if (latest > prevValue && latest > 30) {
+            setHidden(true);
+        } else {
+            setHidden(false);
+        }
+    });
+
     useEffect(() => {
         const handleClickOutside = (event) => {
             if (isOpen && navbarRef.current && !navbarRef.current.contains(event.target)) {
@@ -58,46 +83,46 @@ const Navbar = () => {
     const links = (
         <>
             <NavLink to={"/"} className={({ isActive }) =>
-                isActive
-                    ? "border-b-2 border-black font-bold"
-                    : " "
+                isActive ? "border-b-2 border-black font-bold" : " "
             }>
                 Home
             </NavLink>
             <NavLink to={"/men"} className={({ isActive }) =>
-                isActive
-                    ? "border-b-2 border-black font-bold"
-                    : " "
+                isActive ? "border-b-2 border-black font-bold" : " "
             }>
                 Men
             </NavLink>
             <NavLink to={"/women"} className={({ isActive }) =>
-                isActive
-                    ? "border-b-2 border-black font-bold"
-                    : " "
+                isActive ? "border-b-2 border-black font-bold" : " "
             }>
                 Women
             </NavLink>
         </>
-    )
+    );
+
+
     return (
-        <>
-            <div className="navbar flex justify-between px-0 bg-transparent py-6 text-black" ref={navbarRef}>
+        <motion.div
+            variants={{
+                visible: { y: 0 },
+                hidden: { y: "-100%" },
+            }}
+            animate={hidden ? "hidden" : "visible"}
+            transition={{ duration: 0.35, ease: "easeInOut" }}
+            className="fixed top-0 left-0 w-full z-50 bg-white"
+        >
+            <div className="navbar flex justify-between px-6 py-6 text-black" ref={navbarRef}>
                 <div>
                     <div className="dropdown hover:cursor-pointer">
                         <div tabIndex={0} className="pr-4 lg:hidden" onClick={toggleMenu}>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <HiMenuAlt4 size={22} />
-                            </svg>
+                            <HiMenuAlt4 size={22} />
                         </div>
                         <ul className={`menu menu-sm dropdown-content mt-3 z-[50] p-2 shadow-sm w-40 space-y-3 ${isOpen ? "translate-x-0" : "-translate-x-96"} bg-white border border-black transition-transform duration-300 ease-in-out`}>
                             {links}
                         </ul>
                     </div>
                     <Link to={"/"}>
-                        <p className="text-xl font-bold">
-                            Thread Frenzy
-                        </p>
+                        <p className="text-xl font-bold">Thread Frenzy</p>
                     </Link>
                 </div>
                 <div className="hidden lg:flex">
@@ -105,7 +130,7 @@ const Navbar = () => {
                         {links}
                     </ul>
                 </div>
-                <div className=" flex gap-4">
+                <div className="flex gap-4">
                     <div className="block hover:cursor-pointer">
                         {theme === "dark" ? (
                             <BsSun onClick={handleTheme} size={22} />
@@ -113,26 +138,32 @@ const Navbar = () => {
                             <BsMoonStars onClick={handleTheme} size={22} />
                         )}
                     </div>
-                    <Link to={'/cart'}><div className=" hover:cursor-pointer">
-                        <IoBagOutline size={25} />
-                    </div></Link>
-                    {
-                        user
-                            ? <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
-                                <div tabIndex={0} role="button">
-                                    <AiOutlineUser size={25} />
-                                </div>
-                                <ul tabIndex={0} className="dropdown-content menu z-[50] p-3 shadow bg-white w-44 space-y-3 border border-black">
-                                    <h1 className=" font-semibold text-lg">{user.displayName}</h1>
-                                    <Link to={'/dashboard/profile'} className=" border-y border-gray-300 hover:border-y hover:border-black py-1 text-start"><button>Profile</button></Link>
-                                    <button onClick={handleUserSignOut} className=" bg-black text-white py-1 w-full">Sign Out</button>
-                                </ul>
+                    <Link to={'/cart'}>
+                        <div className="hover:cursor-pointer">
+                            <IoBagOutline size={25} />
+                        </div>
+                    </Link>
+                    {user ? (
+                        <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
+                            <div tabIndex={0} role="button">
+                                <AiOutlineUser size={25} />
                             </div>
-                            : <Link to={'/sign-in'}><button className=" bg-black text-white px-2 py-1 ">Sign In</button></Link>
-                    }
+                            <ul tabIndex={0} className="dropdown-content menu z-[50] p-3 shadow bg-white w-44 space-y-3 border border-black">
+                                <h1 className="font-semibold text-lg">{user.displayName}</h1>
+                                <Link to={'/dashboard/profile'} className="border-y border-gray-300 hover:border-y hover:border-black py-1 text-start">
+                                    <button>Profile</button>
+                                </Link>
+                                <button onClick={handleUserSignOut} className="bg-black text-white py-1 w-full">Sign Out</button>
+                            </ul>
+                        </div>
+                    ) : (
+                        <Link to={'/sign-in'}>
+                            <button className="bg-black text-white px-2 py-1">Sign In</button>
+                        </Link>
+                    )}
                 </div>
             </div>
-        </>
+        </motion.div>
     );
 };
 

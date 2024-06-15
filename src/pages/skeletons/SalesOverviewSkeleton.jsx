b5f8554664b6b7bdfc2861a518519a1d6b7e5230
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-16 00:19:54 +0600

    sales overview

--- src/pages/skeletons/SalesOverviewSkeleton.jsx
+++ src/pages/skeletons/SalesOverviewSkeleton.jsx
@@ -1,10 +1,19 @@
-
-
 const SalesOverviewSkeleton = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="space-y-6">
+                <div className="h-40 w-full bg-gray-300 animate-pulse pl-10 pt-6 flex gap-4 items-center">
+                    <div className="w-1/4 h-10 bg-gray-400 animate-pulse"></div>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className=" h-[480px] bg-gray-300 animate-pulse"></div>
+                    <div className=" h-[480px] bg-gray-300 animate-pulse"></div>
+                    <div className=" h-[480px] bg-gray-300 animate-pulse"></div>
+                    <div className=" h-[480px] bg-gray-300 animate-pulse"></div>
+                </div>
+            </div>
+        </>
     );
 };
 

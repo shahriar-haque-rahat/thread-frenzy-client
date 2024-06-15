7a1f2426b6d41e800ae7cf69094aae76e65e96f8
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-16 00:08:42 +0600

    features

--- src/pages/home/Features.jsx
+++ src/pages/home/Features.jsx
@@ -1,17 +1,24 @@
-import features from "../../json/features.json";
+import Adidas from "../../assets/images/brandLogo/Adidas.png"
+import AllenSolly from "../../assets/images/brandLogo/Allen Solly.png"
+import BuckMason from "../../assets/images/brandLogo/Buck Mason.png"
+import CalvinKlein from "../../assets/images/brandLogo/Calvin Klein.png"
+import Everlane from "../../assets/images/brandLogo/Everlane.png"
+import Lacoste from "../../assets/images/brandLogo/Lacoste.png"
+import Levis from "../../assets/images/brandLogo/Levis.png"
+import Nike from "../../assets/images/brandLogo/Nike.png"
 
 const Features = () => {
     return (
         <>
             <div className=" grid grid-cols-4 md:grid-cols-8 gap-4 justify-center items-center p-[5%] h-full bg-white">
-                {
-                    features?.map((item, idx) => (
-                        <div key={idx}>
-                                <img src={item.image} alt={item.brand}/>
-                        </div>
-
-                    ))
-                }
+                <img src={Lacoste} alt="Lacoste" />
+                <img src={Adidas} alt="Adidas" />
+                <img src={AllenSolly} alt="AllenSolly" />
+                <img src={BuckMason} alt="BuckMason" />
+                <img src={CalvinKlein} alt="CalvinKlein" />
+                <img src={Everlane} alt="Everlane" />
+                <img src={Nike} alt="Nike" />
+                <img src={Levis} alt="Levis" />
             </div>
         </>
     );

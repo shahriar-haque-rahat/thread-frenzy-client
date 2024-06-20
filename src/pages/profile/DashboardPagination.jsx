9ba591c6404fd8022080432bcc11e85e32798df6
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-21 01:50:06 +0600

    deploy

--- src/pages/profile/DashboardPagination.jsx
+++ src/pages/profile/DashboardPagination.jsx
@@ -4,16 +4,21 @@ const DashboardPagination = ({ currentPage, totalPages, handlePageChange }) => {
 
 
     return (
-        <div className="flex justify-center mt-4">
-            <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className={currentPage === 1 ? " px-4 py-2 border bg-gray-200 text-black" : "px-4 py-2 border"}>
-                <RiArrowLeftSLine size={20} />
-            </button>
+        <div>
+            {
+                totalPages === 0 ? <div></div>
+                    : <div className="flex justify-center mt-4">
+                        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1} className={currentPage === 1 ? " px-4 py-2 border bg-gray-200 text-black" : "px-4 py-2 border"}>
+                            <RiArrowLeftSLine size={20} />
+                        </button>
 
-            <span className="px-4 py-2 w-32 text-center">{`Page ${currentPage} of ${totalPages}`}</span>
+                        <span className="px-4 py-2 w-32 text-center">{`Page ${currentPage} of ${totalPages}`}</span>
 
-            <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className={currentPage === totalPages ? " px-4 py-2 border bg-gray-200 text-black" : "px-4 py-2 border"}>
-                <RiArrowRightSLine size={20} />
-            </button>
+                        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages} className={currentPage === totalPages ? " px-4 py-2 border bg-gray-200 text-black" : "px-4 py-2 border"}>
+                            <RiArrowRightSLine size={20} />
+                        </button>
+                    </div>
+            }
         </div>
     );
 };

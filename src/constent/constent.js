cae8335584aedb68162487dbd94ad90cbcf10055
shRahat10 <shahriar.haque.1011@gmail.com>
2024-06-20 23:35:33 +0600

    deploy

--- src/constent/constent.js
+++ src/constent/constent.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://localhost:5000'
-export const BASE_URL = 'http://localhost:5000/v1/api'
+// export const BASE_URL = 'http://localhost:5000/v1/api'
+export const BASE_URL = 'https://thread-frenzy-server.vercel.app/v1/api'
